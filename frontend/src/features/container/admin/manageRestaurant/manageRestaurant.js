6424c520660573e5b91b91da47666604ae69e744
Lan Cong Dang <lancongdangf88@gmail.com>
2022-11-30 22:02:13 +0700

    Detail restaurant admin UI

--- frontend/src/features/container/admin/manageRestaurant/manageRestaurant.js
+++ frontend/src/features/container/admin/manageRestaurant/manageRestaurant.js
@@ -1,15 +1,50 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, useHistory, useRouteMatch } from 'react-router-dom';
+import { Button } from 'antd';
 
+import adminApi from '../../../../api/admin';
 import './tintuc.css'
 
 function ManageRestaurantRoleAdmin() {
     const [ restaurants, setRestaurants ] = useState([]);
-
+    const getRestaurants = async() => {
+        const listRestaurant = await adminApi.getAll();
+        if(listRestaurant) setRestaurants(listRestaurant.data);
+    }
+    useEffect(() => {
+        getRestaurants();
+    }, [])
+    let listRestaurant;
+    if (restaurants) {
+        listRestaurant = restaurants.map((item) => {
+            return (
+                <div className='containerItem'>
+                    <div className='containerImage'>
+                        <img src=''/>
+                    </div>
+                    <div className='containerContent'>
+                        <p>Name: {item.name}</p>
+                        <p>Address: {item.address}</p>
+                        <p>Số điện thoại: {item.phoneNumber}</p>
+                        <Button type='primary'>Xem chi tiết</Button>
+                    </div>
+                </div>
+            )
+        });
+    };
     return (
         <div>
-            <p>Xin chao</p>
+            {restaurants === 0 ? 
+                (
+                    <div>Danh sách trống</div>
+                ) :
+                (
+                    <div>
+                        {listRestaurant}
+                    </div>
+                )
+        }
         </div>
     )
 }
