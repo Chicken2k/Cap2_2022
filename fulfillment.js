7d61f80f4809c7adff44141a28ab3d9eea3b4df4
LanDangCong <quy.tran@smartdev.com>
2022-12-01 00:36:09 +0700

    update package

--- fulfillment.js
+++ fulfillment.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const { WebhookClient } = require('dialogflow-fulfillment')
+// const express = require('express')
+// const { WebhookClient } = require('dialogflow-fulfillment')
 
-const router = express.Router()
+// const router = express.Router()
 
-router.post('/', (req, res) => {
-  const agent = new WebhookClient({ request: req, response: res })
-  let intentMap = new Map()
-  agent.handleRequest(intentMap)
-})
+// router.post('/', (req, res) => {
+//   const agent = new WebhookClient({ request: req, response: res })
+//   let intentMap = new Map()
+//   agent.handleRequest(intentMap)
+// })
 
-module.exports = router
+// module.exports = router
--- server.js
+++ server.js
@@ -56,32 +56,32 @@ app.post("/sendemail", jsonParser, urlEncoded, async (req, res) => {
       console.log(err.statusCode);
     });
 });
-app.post("/chatbot", jsonParser, urlEncoded, async (req, res) => {
-  const message = req.body.message;
-  const userId = req.body.userId;
-  console.log("message" + message);
+// app.post("/chatbot", jsonParser, urlEncoded, async (req, res) => {
+//   const message = req.body.message;
+//   const userId = req.body.userId;
+//   console.log("message" + message);
 
-  try {
-    const response = await talkToChatbot(message, userId);
-    res.send({ message: response });
-  } catch (err) {
-    console.log("Something went wrong: " + err);
-    res.send({
-      error: "Error occured here",
-    });
-  }
-  // talkToChatbot(message)
-  //   .then((response) => {
-  //     res.send({ message: response })
-  //   })
-  //   .catch((error) => {
-  //     console.log('Something went wrong: ' + error)
-  //     res.send({
-  //       error: 'Error occured here',
-  //     })
-  //   })
-});
-app.use(fulfillmentRoutes);
+//   try {
+//     const response = await talkToChatbot(message, userId);
+//     res.send({ message: response });
+//   } catch (err) {
+//     console.log("Something went wrong: " + err);
+//     res.send({
+//       error: "Error occured here",
+//     });
+//   }
+//   // talkToChatbot(message)
+//   //   .then((response) => {
+//   //     res.send({ message: response })
+//   //   })
+//   //   .catch((error) => {
+//   //     console.log('Something went wrong: ' + error)
+//   //     res.send({
+//   //       error: 'Error occured here',
+//   //     })
+//   //   })
+// });
+// app.use(fulfillmentRoutes);
 // app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
