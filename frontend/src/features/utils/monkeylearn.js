a6303c45adfcf7703060b65df52284d3518a2374
QUeen of Year <kurochan159357@gmail.com>
2022-12-13 23:12:49 +0700

    fix comment analyze

--- frontend/src/features/utils/monkeylearn.js
+++ frontend/src/features/utils/monkeylearn.js
@@ -1,26 +1,29 @@
-const axios = require('axios');
-const MonkeyLearn = require('monkeylearn')
+const axios = require("axios");
+const MonkeyLearn = require("monkeylearn");
 // Use the API key from your account
-const ml = new MonkeyLearn('0f57fb9be4617a9e76a06c310680613ea760378b');
+const ml = new MonkeyLearn("9b11fda4c726261af5301dc2399ea1b1012ec25d");
 
 module.exports = {
-    monkeyLearnAnalysis: async (comment) => {
-        // Sentiment analysis ID
-        let model_id = 'cl_pi3C7JiL'
-        let data = []
-        var commentAnalyzed, analyzeComment;
-        data.push(comment);
-        analyzeComment = await ml.classifiers.classify(model_id, data).then(async (response) => {
-            const res = await response.body;
-            res.forEach((data) => {
-                data.classifications.forEach(data => {
-                    commentAnalyzed = data.tag_name;
-                })
-            })
-            return commentAnalyzed;
-        }).catch(error => {
-            console.log(error.response)
-        })
-        return analyzeComment;
-    }
-}
+  monkeyLearnAnalysis: async (comment) => {
+    // Sentiment analysis ID
+    let model_id = "cl_NDBChtr7";
+    let data = [];
+    var commentAnalyzed, analyzeComment;
+    data.push(comment);
+    analyzeComment = await ml.classifiers
+      .classify(model_id, data)
+      .then(async (response) => {
+        const res = await response.body;
+        res.forEach((data) => {
+          data.classifications.forEach((data) => {
+            commentAnalyzed = data.tag_name;
+          });
+        });
+        return commentAnalyzed;
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
+    return analyzeComment;
+  },
+};
